c0e1917af5d919d71541bffdc01ac64b98a6873c
Ian Cullen <ian.james.cullen@gmail.com>
2017-08-01 22:49:29 +0100

    Initial commit - add the scripts

---
+++ create_geant_cmake.sh
@@ -0,0 +1,200 @@
+#!/bin/bash
+
+#####################
+#
+# Script to automate the creation of the cmake command
+#
+# There are many arguments provided to cmake, so to keep
+# consistency between versions, this script only requires:
+#    - version number
+#    - source code location
+#    - build type
+#
+# The actual cmake command is not executed, but a command file
+# is output which can be checked and executed.
+#
+#####################
+
+#ROOT of the install location
+INSTALLDIR=/usr/local/share/geant4
+#Where do the datasets live
+DATAPATH=${INSTALLDIR}/data
+
+#Common options, independent of build type
+BASE_TEMPLATE="-DCMAKE_INSTALL_PREFIX=INSTALLDIR/VERSION/install/BUILD_LEVEL \
+-DGEANT4_INSTALL_DATADIR=DATAPATH \
+-DGEANT4_BUILD_MULTITHREADED=ON "
+
+#Options for the debug build type
+DEBUG_OPTIONS="${BASE_TEMPLATE} \
+-DCMAKE_BUILD_TYPE=RelWithDebInfo \
+-DGEANT4_USE_QT=ON \
+-DGEANT4_USE_GDML=ON \
+-DGEANT4_USE_OPENGL_X11=ON \
+-DGEANT4_USE_RAYTRACER_X11=ON"
+
+#Options for the release build type
+RELEASE_OPTIONS="${BASE_TEMPLATE} \
+-DCMAKE_BUILD_TYPE=Release \
+-DGEANT4_BUILD_STORE_TRAJECTORY=OFF \
+-DGEANT4_BUILD_VERBOSE_CODE=OFF"
+
+############################
+# Don't edit below this line
+############################
+
+RESTORE="\e[0m" #N.B. This is a reset of the colour, not black
+RED="\e[31m"
+GREEN="\e[32m"
+BLUE="\e[1;34m"
+YELLOW="\e[33m"
+
+usage() {
+    echo -e "
+\t${BLUE}USAGE:${RESTORE}\t${0##*/} -v <geant4 version> -s <path to source> -b <release/debug>
+"
+    exit 1
+}
+
+#Check user can write (i.e. build) to where we are.
+#If it can't then exit as we can't go any further
+if [[ ! -w ${PWD} ]]
+then
+    echo -e "\n\t${RED}ERROR:${RESTORE} Current user does not have permission to write here ${PWD}"
+    usage
+fi
+
+#The version, source and build type are required
+if [[ $# -ne 6 ]]
+then
+    echo -e "\n\t${RED}ERROR:${RESTORE}\tWrong number of arguments supplied."
+    usage
+fi
+
+while getopts ":hv:b:s:" OPTIONS
+do
+    case "${OPTIONS}" in
+        h | \? | : )
+            usage
+            ;;
+        v )
+            #Value is validated later in script
+            VERSION=${OPTARG}
+            ;;
+        b )
+            #Value is validated later in script
+            BUILD_LEVEL=${OPTARG}
+            ;;
+        s )
+            #Check the directory exists
+            SOURCE=$(readlink -f "${OPTARG}")
+            [ -d "${SOURCE}" ] || usage
+            ;;
+    esac
+done
+
+#Sanity check version number
+#They changed the CMakeLists.txt file for 10.4 so we need a different way to extract the version
+#Comparing version numbers in a general/distro agnostic way is a pain
+# 1 Make a list containing the input version and the last version prior to the change
+# 2 Sort this list using the inbuilt 'version comparison' flag and extract the 'highest' number
+# 3 Compare this value to the last version prior to the change and act accordingly
+
+#We are doing a string comparison so use a value higher than was ever release, but lower than the change version
+MAX_OLD_VERSION="10.3.9"
+CMAKEFILE=${SOURCE}/CMakeLists.txt
+if [[ "$(echo -e "${VERSION}\n${MAX_OLD_VERSION}" | sort -rV | head -n1)" == "${MAX_OLD_VERSION}" ]]
+then
+    #echo "Pre 10.4"
+    SOURCE_VERSION=$(sed -n 's/^.*_VERSION \"\(.*\)\".*$/\1/p' "${CMAKEFILE}")
+else
+    #echo "Post 10.4"
+    SOURCE_VERSION=$(sed -n 's/^.*PROJECT_NAME._VERSION_.* \([[:digit:]]*\)).*$/\1/p' "${CMAKEFILE}" | paste -s -d '.')
+fi
+
+if [[ "${SOURCE_VERSION}" != "${VERSION}" ]]
+then
+    echo -e "\n\t${RED}WARNING:${RESTORE}\tThe version specified <${VERSION}> does not match that of the source code <${SOURCE_VERSION}>\n"
+
+    while true
+    do
+	read -p "Do you wish to continue? [y/n]: " yn
+	case $yn in
+            [Yy]* )
+		break
+		;;
+            [Nn]* )
+		echo "Exiting ..."
+		exit 3
+		;;
+            * )
+		echo "Please answer yes or no."
+		;;
+	esac
+    done
+fi
+
+
+#Set the cmake options
+case "${BUILD_LEVEL}" in
+    "debug" )
+        OPTIONS_TEMPLATE="${DEBUG_OPTIONS}"
+        ;;
+    "release" )
+        OPTIONS_TEMPLATE="${RELEASE_OPTIONS}"
+        ;;
+    * )
+        echo -e "\n\t${RED}ERROR:${RESTORE}\t${BUILD_LEVEL} is not a valid build type, used 'release' or 'debug' \n"
+        exit 4
+        ;;
+esac
+
+#Arguments have been checked and validated, lets create the command.
+CMAKE_TEMPLATE="cmake ${OPTIONS_TEMPLATE} SOURCE"
+
+echo -e "
+The build directory will be the current directory - ${BLUE}${PWD}${RESTORE}
+
+Creating cmake command using the details
+=========================
+geant4 version         - ${GREEN}${VERSION}${RESTORE}
+geant4 build type      - ${GREEN}${BUILD_LEVEL}${RESTORE}
+root install directory - ${GREEN}${INSTALLDIR}/${VERSION}/install/${BUILD_LEVEL}${RESTORE}
+geant4 data directory  - ${GREEN}${INSTALLDIR}/data${RESTORE}
+source code directory  - ${GREEN}${SOURCE}${RESTORE}
+=========================
+"
+
+#Modify template to use the real values
+#Need to use '%' as the sed delimiter because
+# | are read as command pipes
+# / Are contained within the paths
+#Final substitution squashed muliple space into a single one
+echo -e "\nConstructing the following cmake command:"
+MYMAKE=$(echo "${CMAKE_TEMPLATE}" | \
+                sed -e s%VERSION%${VERSION}%g \
+                    -e s%INSTALLDIR%${INSTALLDIR}%g \
+                    -e s%DATAPATH%${DATAPATH}%g \
+                    -e s%BUILD_LEVEL%${BUILD_LEVEL}%g \
+                    -e s%SOURCE%${SOURCE}% \
+                    -e 's% \+% %g'
+      )
+
+echo -e "${YELLOW}${MYMAKE// /\\n}${RESTORE}"
+
+OUTFILE=${VERSION}_cmake.txt
+echo -en "\nWriting the command into ${OUTFILE} ... "
+echo "${MYMAKE}" > "${OUTFILE}"
+chmod u+x "${OUTFILE}"
+echo -e " done\n"
+
+echo -e "The cmake command can now be run by executing: ${RED}./${OUTFILE}${RESTORE}"
+
+#Check if current user has permissions to 'make install'
+if [[ ! -w ${INSTALLDIR} ]]
+then
+    echo -e "\n${BLUE}INFO:${RESTORE} Current user does not have write permissions to install into ${INSTALLDIR}"
+fi
+
+echo ""
+exit $?
---
+++ run_geant_cmake.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+#####################
+#
+# Script to execute the provided cmake command and download datasets if required
+#
+# New versions of geant4 often come with new datasets. This script automates their
+# download and extraction by running the cmake command file created by
+# create_geant_cmake.sh, parsing the output and doing what's needed.
+#
+#####################
+
+RESTORE="\e[0m" #N.B. This is a reset of the colour, not black
+RED="\e[31m"
+GREEN="\e[32m"
+BLUE="\e[1;34m"
+YELLOW="\e[33m"
+
+usage() {
+    echo -e "
+\t${BLUE}USAGE:${RESTORE}\t${0##*/} -f <cmake command file>
+"
+    exit 1
+}
+
+while getopts ":hf:" OPTIONS
+do
+    case "${OPTIONS}" in
+        h | \? | : )
+            usage
+            ;;
+        f )
+            FILE=${OPTARG}
+            ;;
+    esac
+done
+
+#Confirm that an input file has been provided
+if [[ -z "${FILE}" ]]
+then
+    echo -e "\n\t${RED}ERROR:${RESTORE}\tYou need to specify the cmake command file to run"
+    usage
+elif [[ ! -e "${FILE}" ]]
+then
+    echo -e "\n\t${RED}ERROR:${RESTORE}\t${FILE} does not exist"
+    usage
+fi
+
+#Capture where the data files are expected to live
+DATADIR=$(sed "s/^.*INSTALL_DATADIR=\([^ ]*\).*$/\1/" ${FILE})
+
+if [[ -z ${DATADIR} ]]
+then
+    echo -e "${RED}\n\tERROR:${RESTORE}\tThe data directory has not been specified, exiting...\n"
+    exit 2
+fi
+
+#We'll capture the output rather than running cmake multiple times.
+CMAKE_OUTPUT=cmake_output.log
+
+#Run the cmake command that was provided
+echo -e "${YELLOW}Running cmake${RESTORE}\n"
+
+#If cmake fails, we have bigger issues than missing data
+if ! ./${FILE} 2>&1 | tee ${CMAKE_OUTPUT}
+then
+    echo -e "${RED}\n\tERROR:${RESTORE}\tRunning cmake failed"
+    exit 3
+fi
+
+#What should we look for to confirm that we are missing datasets
+MISSING_STRING="the following datasets are NOT present"
+
+#If we aren't missing any datasets, there is nothing further to do.
+if [[ $(grep -c "${MISSING_STRING}" ${CMAKE_OUTPUT}) -eq 0 ]]
+then
+    echo -e "\n${GREEN}All went as expected.${RESTORE}\n"
+    exit 4
+#But if we are, make sure we can write/extract to the path specified
+else
+    echo -en "\n${YELLOW}Data files are missing "
+
+    if [[ ! -w ${DATADIR} ]]
+    then
+        echo -e "${YELLOW}but you don't have permission to write to ${DATADIR}, exiting...\n${RESTORE}"
+        exit 5
+    fi
+
+    echo -e "aquiring now.${RESTORE}\n"
+fi
+
+#Store the url(s) of the missing datasets
+FILE_LIST=${DATADIR}/filesToGet.txt
+#The commonly unique part of the url in the cmake output
+FILE_STRING="http://geant4.cern.ch/support/source"
+
+#Get which data files are missing.
+sed -n "s|^[ \t]*\(${FILE_STRING}\)|\1|p" ${CMAKE_OUTPUT} > ${FILE_LIST}
+
+echo -e "${YELLOW}Moving into the data directory ${DATADIR}${RESTORE}\n"
+pushd "${DATADIR}" > /dev/null
+
+echo -e "${YELLOW}Downloading and extracting the necessary files:${RESTORE}"
+#We could pass the file to wget with the -i flag but we'd still have to parse
+#it to extract each dataset so do it this way
+while read -r LINE
+do
+    #echo "${LINE} |---| ${LINE##*/}"
+    #We want to keep the archived files so download and extract separately
+    wget -nv --show-progress ${LINE}
+    #Without the 'o' flag, files extract as owned by geant developers i.e. unknown on this system
+    tar xof ${LINE##*/}
+done < ${FILE_LIST}
+
+#Cleanup, delete, the file with list of missing datasets
+rm -f ${FILE_LIST}
+
+#We could do a recursive call to this script, but lets not.
+#Just re-run the cmake command, everything should work now.
+echo -e "\n${YELLOW}Re-running cmake now that we have the all of the datasets${RESTORE}"
+popd > /dev/null
+./${FILE}
+
+#Leave the output from the original cmake execution
+#rm -v ${CMAKE_OUTPUT}
+
+exit $?
